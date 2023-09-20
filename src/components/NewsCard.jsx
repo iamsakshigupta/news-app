0f41d2b185c5da078905e7eae0fb84f39c644bae
iamsakshigupta <107625238+iamsakshigupta@users.noreply.github.com>
2023-09-20 06:10:10 +0530

    headline section added

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -5,14 +5,30 @@ export class NewsCard extends Component {
   render() {
     return (
       <div>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+        {/*API Key 9796962f8b0f43398523fd4a4b557f70 */}
+        <div className='max-w-7xl px-4 mx-auto py-5 flex flex-col'>
+          <h2 className='font-bold text-2xl'>Top Headlines</h2>
+          <div className='flex justify-between '>
+            <div className=''>
+              <NewsItem />
+            </div>
+            <div className=''>
+              <NewsItem />
+            </div>
+            <div className=''>
+              <NewsItem />
+            </div>
+          </div>
+          
+          
+          
+        </div>
+        
+
+
 
       </div>
-      
+
     )
   }
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,8 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description}=this.props
     return (
-      <div>NewsItem</div>
+      <div>
+        <div className="bg-white rounded-lg shadow-lg p-4">
+          <h2 className="text-xl font-semibold">{title}</h2>
+          <p className="text-gray-600">{description}</p>
+          <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-lg hover:bg-blue-600">
+            Learn More
+          </button>
+        </div>
+      </div>
     )
   }
 }
