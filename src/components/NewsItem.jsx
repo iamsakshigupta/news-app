20013d1bbf09725a786e4f76da76bcd088035d87
iamsakshigupta <107625238+iamsakshigupta@users.noreply.github.com>
2023-09-21 11:30:58 +0530

    newsApi used

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl } = this.props
+    let { title, description, imageUrl, newsUrl } = this.props
     return (
       <div>
         <div className='bg-white rounded-lg shadow-lg p-4 h-full'>
@@ -16,10 +16,14 @@ export class NewsItem extends Component {
             </div>
             <div className='flex-grow'>
               <h2 className='text-xl font-semibold mt-3'>{title}</h2>
-              <p className='text-gray-600 mt-3'>{description}</p>
-              <button className='bg-blue-500 text-white px-4 py-2 mt-5 rounded-lg hover:bg-blue-600'>
-                Learn More
-              </button>
+              <p className='text-gray-600 mt-3 mb-7'>{description}</p>
+              
+              <a className='bg-blue-500 text-white px-4 py-2 mt-5 rounded-lg hover:bg-blue-600' href={newsUrl} target='_blank'>
+                Read More
+              </a>
+              {/* <button className='bg-blue-500 text-white px-4 py-2 mt-5 rounded-lg hover:bg-blue-600'>
+                <a href={newsUrl} target='_blank'>Read More</a>
+              </button> */}
             </div>
           </div>
         </div>
